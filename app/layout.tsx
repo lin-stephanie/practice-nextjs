52aaf6257a91c876772b5abfda3caa39e4b2a034
椰云拿铁 <102728558+lin-stephanie@users.noreply.github.com>
2024-04-04 20:01:40 +0800

    Chapter 2: CSS Styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+// You can import global.css in any component in your application,
+// but it's usually good practice to add it to your top-level component.
+// In Next.js, this is the root layout (more on this later).
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
